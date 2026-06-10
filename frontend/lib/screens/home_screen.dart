498ecb1e3685dde13d5110c70155dd743e123c72
viniciusbtech <viniciusbtech@gmail.com>
2026-06-09 21:28:31 -0300

    Issue#1Vinicius

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,79 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../pages/biblioteca_materiais_page.dart';
+import '../widgets/app_scaffold.dart';
 import 'agenda_screen.dart';
-import 'materiais_screen.dart';
+import 'atividades_screen.dart';
 import 'consistencia_screen.dart';
 import 'ciclo_estudos_screen.dart';
 import 'configuracoes_screen.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
-
-  late final List<Widget> _screens;
-
-  @override
-  void initState() {
-    super.initState();
-    _screens = [
-      const AgendaScreen(),
-      const MateriaisScreen(),
-      const ConsistenciaScreen(),
-      const CicloEstudosScreen(),
-      const ConfiguracoesScreen(),
-    ];
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _screens,
-      ),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _currentIndex,
-        onDestinationSelected: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        elevation: 3,
-        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.calendar_today_outlined),
-            selectedIcon: Icon(Icons.calendar_month_rounded),
-            label: 'Agenda',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.folder_open_outlined),
-            selectedIcon: Icon(Icons.folder_rounded),
-            label: 'Materiais',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.track_changes_outlined),
-            selectedIcon: Icon(Icons.track_changes_rounded),
-            label: 'Consistência',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.view_week_outlined),
-            selectedIcon: Icon(Icons.view_week_rounded),
-            label: 'Ciclos',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.settings_outlined),
-            selectedIcon: Icon(Icons.settings_suggest_rounded),
-            label: 'Ajustes',
-          ),
-        ],
-      ),
+    return const AppScaffold(
+      screens: [
+        AgendaScreen(),
+        AtividadesScreen(),
+        BibliotecaMateriaisPage(),
+        ConsistenciaScreen(),
+        CicloEstudosScreen(),
+        ConfiguracoesScreen(),
+      ],
     );
   }
 }
